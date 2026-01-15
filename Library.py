b6103b45d891b2e4b8ed5aa1e944a819d0792bdb
Alex <alexanderraaguirre@gmail.com>
2026-01-15 11:19:10 -0600

    okay

--- Library.py
+++ Library.py
@@ -1,14 +1,27 @@
 import json
-class library:
+from Book import Book
+class Library:
 
     def __init__(self):
-        self.book = []
+        self.books = []
+        
         with open("collection.json", "r") as file:
             collection = json.load(file)
+            
             for book_dict in collection:
-                book_obj = ()
-        
+                book_obj = Book(
+                book_check="json",
+                book_id=0,
+                title=book_dict["title"],
+                 author=book_dict["author"],
+                pages=book_dict["pages"],
+                genre=book_dict["genre"],
+                available_copies=book_dict["total_copies"],
+                total_copies=book_dict["total_copies"]
+                )
+
+                self.books.append(book_obj)
+
     
     
-    def book(self,books):
-        return books = books_list 
+    
